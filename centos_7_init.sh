b44044b0fc2afe8f48456b037855668bb69d0605
viacooky <viacooky@qq.com>
2017-05-05 16:18:39 +0800

    Update centos_7_init.sh

--- centos_7_init.sh
+++ centos_7_init.sh
@@ -6,7 +6,7 @@ yum install -y wget
 wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-7.repo
 yum clean all
 yum makecache
-yum update
+yum update -y
 yum install -y epel-release git net-tools zsh htop saidar
 sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 wget -O ~/.zshrc https://raw.githubusercontent.com/viacooky/linux_init_shell/master/.zshrc
