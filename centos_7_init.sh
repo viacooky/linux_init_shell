5fec0a34fc8b03644dfd4e1f57c66a447c346440
viacooky <viacooky@qq.com>
2017-05-05 16:12:54 +0800

    Update centos_7_init.sh
    up

--- centos_7_init.sh
+++ centos_7_init.sh
@@ -3,7 +3,7 @@
 # 添加net-tools
 cp /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.bak
 yum install -y wget
-wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-7.rep
+wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-7.repo
 yum clean all
 yum makecache
 yum update
