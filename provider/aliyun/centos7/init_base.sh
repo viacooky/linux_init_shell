330b504b02e902f7ed3135940386b6439d02ac37
viacooky <viacooky@qq.com>
2019-03-05 12:04:00 +0800

    [FIX] 处理 oh my zsh 安装完成后问题
    解决 oh my zsh 的 install.sh 在完成后，直接进入zsh环境，影响剩余命令执行的问题

--- provider/aliyun/centos7/init_base.sh
+++ provider/aliyun/centos7/init_base.sh
@@ -4,10 +4,10 @@ echo "
 Aliyun CentOS 7 初始化脚本
 
 即将执行如下操作：
-    1. 替换yum源为阿里云
-    2. 安装必备工具包(epel-release git net-tools)
-    3. 安装zsh
-    4. 清理AliYun国内版后台服务(需要重启)
+    * 替换yum源为阿里云
+    * 安装必备工具包(epel-release git net-tools)
+    * 清理AliYun国内版后台服务(需要重启)
+    * 安装zsh
 "
 
 read -r -p "是否执行?  [Y/n] " input
@@ -23,12 +23,13 @@ case $input in
 
         echo "================== [ 安装 on-my-zsh ] ====================="
         sh -c "$(wget https://github.com/viacooky/shell_script/raw/master/provider/aliyun/centos7/pack/install_zsh.sh -O -)"
-        
+
         echo "================== [ 清理AliYun国内版后台服务 ] ====================="
-        # 脚本来自 https://github.com/FanhuaCloud/AliYunServicesClear
+        # 脚本来自 https://github.com/FanhuaCloud/AliYunServicesClear"
         sh -c "$(wget https://raw.githubusercontent.com/FanhuaCloud/AliYunServicesClear/master/uninstall.sh -O -)"
 
         echo "================== [ 完成 ] ====================="
+        echo "请手工重启..."
     ;;
 
     [nN][oO]|[nN])
--- provider/aliyun/centos7/pack/install_zsh.sh
+++ provider/aliyun/centos7/pack/install_zsh.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
 yum install -y zsh
-sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+# 替换 oh-my-zsh install.sh 中的 env zsh，实现 安装后不进入zsh，继续其他语句
+sh -c "$(wget -O - https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sed 's:env zsh -l::g' | sed 's:chsh -s .*$::g')"
 wget -O ~/.zshrc https://github.com/viacooky/shell_script/raw/master/configs/zsh/.zshrc
 source ~/.zshrc
